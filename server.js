a2e487d32b4046b6e0d76ba1a2112022bd83f56f
Nav <navjot.singh@pepcoding.com>
2021-11-05 19:42:16 +0530

    maintenance Server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+app.listen(PORT, () => console.log(`Running at http://localhost:${PORT}`));
