4a531bff23710caec60cb23c6165f0ecbc4c95a9
Nav <navjot.singh@pepcoding.com>
2021-11-12 11:49:44 +0530

    Updates

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 443;
+const path = require("path");
+app.use(express.static(path.resolve(__dirname, "public")));
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile("index.html");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
 });
+// app.get("/feed/*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public/index.html"));
+// });
 
 app.listen(PORT, () => console.log(`Running at http://localhost:${PORT}`));
